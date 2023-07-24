5114c2955948e600217330e493e9e928e15eced9
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-07-24 15:25:36 +0200

    refactor

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleClearList() {
+    let confirmed = null;
+    if (items.length !== 0) {
+      confirmed = window.confirm("Are you sure you want to delete all items?");
+    }
+
+    if (confirmed) setItems([]);
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
